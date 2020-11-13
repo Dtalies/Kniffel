f33a2938b4ae70676b928df6947598198ed57e7e
dtalies <darwin.talies2013@gmail.com>
2020-11-13 14:57:12 +0100

    Test Oberfläche

---
+++ src/GUI.java
@@ -0,0 +1,39 @@
+import java.awt.*;
+import java.awt.event.*;
+import javax.swing.*;
+
+import jdk.internal.platform.Container;
+
+import java.util.*;
+
+public class GUI {
+    JFrame f;
+    JPanel p;
+    JPanel p2;
+    JPanel p3;
+
+    Container contentPane;
+
+    JLabel l;
+    JLabel l2;
+    JLabel l3;
+    JLabel l4;
+    JLabel l5;
+    JLabel l6;
+    
+    JButton b;
+    JTextField text;
+
+    public main gui()
+    {
+        Test();
+    }
+
+    public Test()
+    {
+
+        
+    }
+    
+
+}
